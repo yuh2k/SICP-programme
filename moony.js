879b12b15dff92646ef53836e5d400e88e3b8214
yuh2k <48446719+yuh2k@users.noreply.github.com>
2021-05-18 16:22:54 +0800

    Update moony.js

--- moony.js
+++ moony.js
@@ -7,13 +7,7 @@ function besiden(n,r){
     return quarter_turn_right(stackn(n, quarter_turn_left(r)));
 }
 
-// function combine(n, shape){
-//     return show(beside_frac(1/5, row(0,shape),                           
-//                 beside_frac(1/4, row(1,shape),
-//                 beside_frac(1/3, row(2,shape),
-//                 beside_frac(1/2, row(3,shape),row(4,shape))))));
-// }
-
+//recursion
 function combine(n,shape){
     return n === 0
     ? row(1/n, shape)
