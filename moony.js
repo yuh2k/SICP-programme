9508380997b077ad4ef60e89c56ded0456390bd0
yuh2k <48446719+yuh2k@users.noreply.github.com>
2021-05-18 16:23:42 +0800

    Update moony.js

--- moony.js
+++ moony.js
@@ -1,3 +1,10 @@
+
+// Staircase
+// The goal of these next exercises is to write a function moony that takes a parameter n and
+// produces a rune with n circles on the steps of a staircase. The picture below is produced by
+// show(moony(5)).
+
+
 function row(n, shape){
     return stack_frac(n/5, blank,
         stack_frac((1 / (5-n)), shape, square));
